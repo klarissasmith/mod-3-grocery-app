6f0e10514988b5b771717f29d084c7f0610cb7b6
klarissasmith <ksmith_email@yahoo.com>
2020-06-16 17:58:20 -0700

    adding render products

--- grocery-frontend 1.55.21 PM/index.js
+++ grocery-frontend 1.55.21 PM/index.js
@@ -11,41 +11,100 @@ const cartContent = document.querySelector(".cart-content")
 const productsDOM = document.querySelector(".products-all")
 
 let cart = []
+function fetchProducts() {
+    fetch(PRODUCTS_URL).then(response => response.json()).then(json => json.forEach(product => {
+        renderProducts(product)
+    }))
+};
+function renderProducts(product) {
+    let article = document.createElement("article")
+    article.className = "product"
+    productsDOM.appendChild(article)
+    
+    let div = document.createElement("div")
+    div.className = "img-container"
+    let productBtn = document.createElement("button")
+    productBtn.className = "bag-btn"
+    productBtn.setAttribute("data-id", "1")
+    let i = document.createElement("i")
+    i.className = "fas fa-shopping-cart"
+    productBtn.appendChild(i)
+    i.innerText = " Add to Cart"
+    div.appendChild(productBtn)
+    let img = document.createElement("img")
+    div.appendChild(img)
+    // img.src = "./images/apples.jpg"
+    img.src = "./images/groceries2.jpg"
+    img.class = "product-img"
+    img.alt = product.name
+    article.appendChild(div)
+    
+    
 
+    let h3 = document.createElement("h3")
+    h3.innerText = product.name
+    article.appendChild(h3)
+
+    //article => div(img-container) => img(src pic), button => 
+    
+
+
+    
+
+  
+    let h4 = document.createElement("h4")
+    h4.innerText = `$${product.price}`
+    article.appendChild(h4)
+}
 document.addEventListener("DOMContentLoaded", () => {
-    const display = new Display();
-    const products = new Products()
-
-
-// get all products
-products.getProducts().then(data => console.log(data.forEach(e => console.log(e.name))))
-//Just testing to see what it will return
-
+    fetchProducts()
+    
 
 
 })
 
 
-//get products
-class Products{
-    async getProducts(){
-        try{
-            let result = await fetch(PRODUCTS_URL)
-            let data = await result.json();
-            let products = data
-            return data;
-        }catch (error){
-            console.log(error);
-        }
-    }
-}
-//display products
-class Display{
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+//---Commented Out Classes ----
+
+// const display = new Display();
+    // const products = new Products()
+
+
+// get all products
+// products.getProducts().then(data => console.log(data.forEach(e => console.log(e.name))))
+//Just testing to see what it will return
+
+//get products
+// class Products{
+//     async getProducts(){
+//         try{
+//             let result = await fetch(PRODUCTS_URL)
+//             let data = await result.json();
+//             let products = data
+//             return data;
+//         }catch (error){
+//             console.log(error);
+//         }
+//     }
+// }
+
+
 
 //storage
 
-class Storage{
+// class Storage{
 
-}
+// }
