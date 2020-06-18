e08b223c77e54631da36b44ce54302ed2c17df3e
Trevor <tlow94@uw.edu>
2020-06-17 17:58:40 -0700

    CRD

--- grocery-frontend 1.55.21 PM/index.js
+++ grocery-frontend 1.55.21 PM/index.js
@@ -1,9 +1,13 @@
 let PRODUCTS_URL = "http://localhost:3000/products";
 let CARTPRODUCTS_URL = "http://localhost:3000/cart_products";
 let CURRENT_CART = 3;
+let addProduct = false;
+let editProduct = false;
 
+const form = document.querySelector(".add-product-form")
 const bagBtn = document.querySelector(".bag-btn");
 const cartBtn = document.querySelector(".cart-btn");
+const productContainer = document.querySelector(".container")
 const closeCartBtn = document.querySelector(".close-cart")
 const emptyCartBtn = document.querySelector(".empty-cart")
 const cartDOM = document.querySelector(".cart")
@@ -13,12 +17,24 @@ const cartItems = document.querySelector(".cart-item")
 const cartTotal = document.querySelector(".cart-total")
 const cartContent = document.querySelector(".cart-content")
 const productsDOM = document.querySelector(".products-all")
+const title = document.querySelector(".section-title")
+const divC = document.createElement("div")
+divC.className = "section-title"
+
+const buttonC = document.createElement("button")
+    buttonC.className = "create-btn"
+    buttonC.innerText = "Create Item"
+    divC.appendChild(buttonC)
+    title.appendChild(divC)
+
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
     // const display = new Display();
     // const products = new Products()
 fetchProducts()
+createProduct()
 
 // get all products
 // products.getProducts().then(data => console.log(data.name))
@@ -35,13 +51,62 @@ closeCartBtn.addEventListener("click", () => {
 emptyCartBtn.addEventListener("click", () => {
     cartItems.remove("cart-content")
 })
-//delete function is not persisting, don't think I can use a POST b/c it would change the displayed items. Maybe local storage or store the products in cart JSON?
 
+
+buttonC.addEventListener("click", () => {
+    // hide & seek with the form
+    addProduct = !addProduct;
+    if (addProduct) {
+      productContainer.style.display = "block";
+    } else {
+      productContainer.style.display = "none";
+    }
+  });
 
 
 })
 
 
+
+function createProduct () {
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+        console.dir(e.target.image)
+        let name = e.target.name.value
+        e.target.name.value = ""
+
+        let price = e.target.price.value
+        e.target.price.value = ""
+
+        let image = e.target.image.value
+        e.target.image.value = ""
+
+        fetch(PRODUCTS_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({"name":name, "price":price, "img_url":image})
+        })
+        .then(response => response.json())
+        .then(json => renderProduct(json))
+        
+        
+
+    })
+}
+
+function deleteProduct(product){
+    fetch(PRODUCTS_URL + "/" + `${product.id}`,{
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        }
+    })
+
+}
 function renderProduct(product) {
     let article = document.createElement("article")
     article.className = "product"
@@ -51,11 +116,92 @@ function renderProduct(product) {
     let img = document.createElement("img")
     let div = document.createElement("div")
     let button = document.createElement("button")
+
+    let buttonD = document.createElement("button")
+    buttonD.addEventListener("click", () => {
+        deleteProduct(product)
+        article.remove()
+    
+    
+    
+    })
+    let buttonE = document.createElement("button")
+    let eForm = document.createElement("form")
+    eForm.className = "edit-product-form"
+    // eForm.innerText = "Edit Product"
+    buttonE.appendChild(eForm)
+
+
+    buttonE.addEventListener("click", () => {
+        // hide & seek with the form
+        editProduct = !editProduct;
+        if (editProduct) {
+          editContainer.style.display = "block";
+        } else {
+          editContainer.style.display = "none";
+        }
+      });
+    
+
+{/* <h2>Groceries</h2>
+        <div class="container">
+            <form class="add-product-form">
+              <h3>Add New Product</h3>
+      
+              <input
+                type="text"
+                name="name"
+                value=""
+                placeholder="Enter a product name..."
+                class="input-text"
+              />
+              <br />
+              <input
+                type="text"
+                name="price"
+                value=""
+                placeholder="Enter a product price..."
+                class="input-text"
+              />
+              <br />
+              <input
+                type="text"
+                name="image"
+                value=""
+                placeholder="Enter a product image URL..."
+                class="input-text"
+              />
+              <br />
+              <input
+                type="submit"
+                name="submit"
+                value="Create New Product"
+                class="submit"
+              />
+            </form> */}
+
+   
+   
+  
+    
+    
+
+    
     let i = document.createElement("i")
+    let i2 = document.createElement("i")
+    let i3 = document.createElement("i")
     i.className = "fas fa-shopping-cart"
+    i2.className = "fas fa-trash"
+    i3.className = "fas fa-edit"
     i.innerText = "add to cart"
+    i2.innerText = "Delete Item"
+    i3.innerText = "Edit Item"
     button.className = "bag-btn"
+    buttonD.className = "dlt-btn"
+    buttonE.className = "edit-btn"
     button.id = product.id
+    buttonD.id = product.id
+    buttonE.id = product.id
     // "add to cart" button
     button.addEventListener("click", () => {
         console.log(`You added ${product.name} to your cart!`);
@@ -89,7 +235,12 @@ function renderProduct(product) {
     div.appendChild(img)
     article.appendChild(div)
         button.appendChild(i)
+        buttonD.appendChild(i2)
+        buttonE.appendChild(i3)
     div.appendChild(button)
+    div.appendChild(buttonD)
+    div.appendChild(buttonE)
+ 
 
     let h3 = document.createElement("h3")
     h3.innerText = product.name
